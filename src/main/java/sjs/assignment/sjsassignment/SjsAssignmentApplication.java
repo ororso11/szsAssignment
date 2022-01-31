f84fc6f9e5850a83e53af1c09fe585200f30b22b
mac <ororso135055@gmail.com>
2022-02-01 02:36:02 +0900

    Initial commit

---
+++ src/main/java/sjs/assignment/sjsassignment/SjsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package sjs.assignment.sjsassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjsAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjsAssignmentApplication.class, args);
+    }
+
+}
